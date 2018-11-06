16de4d635ff6f7fb7088f3e32e3f409b32475323
tormozz48 <tormozz48@gmail.com>
2018-11-06 21:16:28 +0300

    test: Add tests for translations API

---
+++ src/test/translations/translations.ts
@@ -0,0 +1,52 @@
+import * as nock from 'nock';
+import {expect} from 'chai';
+import * as helper from '../helper';
+import LocaliseAPI from '../../index';
+
+const SCOPE = 'translations';
+
+describe(`api/${SCOPE}`, () => {
+    let localiseAPI;
+
+    beforeEach(() => {
+        localiseAPI = new LocaliseAPI({baseUrl: helper.BASE_URL, token: '1234567890'});
+    });
+
+    describe('list', () => {
+        it('should receive list of all translations', async () => {
+            nock(helper.BASE_URL)
+                .get('/projects/abcde.12345/translations')
+                .query({page: 1, limit: 100})
+                .reply(200, helper.getResponse(SCOPE, 'list.json'));
+
+            const result = await localiseAPI.translations.list('abcde.12345', {page: 1, limit: 100});
+            expect(result).to.eql(helper.getResponse(SCOPE, 'list.json'));
+        });
+    });
+
+    describe('get', () => {
+        it('should receive single translation', async () => {
+            nock(helper.BASE_URL)
+                .get('/projects/abcde.12345/translations/567')
+                .reply(200, helper.getResponse(SCOPE, 'get.json'));
+
+            const result = await localiseAPI.translations.get('abcde.12345', 567);
+            expect(result).to.eql(helper.getResponse(SCOPE, 'get.json'));
+        });
+    });
+
+    describe('update', () => {
+        it('should update translation', async () => {
+            nock(helper.BASE_URL)
+                .put(
+                    '/projects/abcde.12345/translations/567',
+                    helper.getRequest(SCOPE, 'update.json')
+                )
+                .reply(200, helper.getResponse(SCOPE, 'update.json'));
+
+            const result = await localiseAPI.translations
+                .update('abcde.12345', 567, helper.getRequest(SCOPE, 'update.json'));
+            expect(result).to.eql(helper.getResponse(SCOPE, 'update.json'));
+        });
+    });
+});
