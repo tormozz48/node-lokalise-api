476ef02de83865ad5900b091d96fd7c9d86b95c9
tormozz48 <tormozz48@gmail.com>
2018-11-10 19:28:23 +0300

    test: Add unit-tests for contributors API

---
+++ src/test/contributors/contributors.ts
@@ -0,0 +1,78 @@
+import * as nock from 'nock';
+import {expect} from 'chai';
+import * as helper from '../helper';
+import LocaliseAPI from '../../index';
+
+const SCOPE = 'contributors';
+
+describe(`api/${SCOPE}`, () => {
+    let localiseAPI;
+
+    beforeEach(() => {
+        localiseAPI = new LocaliseAPI({baseUrl: helper.BASE_URL, token: '1234567890'});
+    });
+
+    describe('list', () => {
+        it('should return list of all contributors', async () => {
+            nock(helper.BASE_URL)
+                .get('/projects/abcde.12345/contributors')
+                .query({page: 1, limit: 100})
+                .reply(200, helper.getResponse(SCOPE, 'list.json'));
+
+            const result = await localiseAPI.contributors.list('abcde.12345', {page: 1, limit: 100});
+            expect(result).to.eql(helper.getResponse(SCOPE, 'list.json'));
+        });
+    });
+
+    describe('get', () => {
+        it('should return single contributor', async () => {
+            nock(helper.BASE_URL)
+                .get('/projects/abcde.12345/contributors/567')
+                .reply(200, helper.getResponse(SCOPE, 'get.json'));
+
+            const result = await localiseAPI.contributors.get('abcde.12345', 567);
+            expect(result).to.eql(helper.getResponse(SCOPE, 'get.json'));
+        });
+    });
+
+    describe('create', () => {
+        it('should create new contributor', async () => {
+            nock(helper.BASE_URL)
+                .post(
+                    '/projects/abcde.12345/contributors',
+                    helper.getRequest(SCOPE, 'create.json')
+                )
+                .reply(200, helper.getResponse(SCOPE, 'create.json'));
+
+            const result = await localiseAPI.contributors
+                .create('abcde.12345', helper.getRequest(SCOPE, 'create.json'));
+            expect(result).to.eql(helper.getResponse(SCOPE, 'create.json'));
+        });
+    });
+
+    describe('update', () => {
+        it('should update existed contributor', async () => {
+            nock(helper.BASE_URL)
+                .put(
+                    '/projects/abcde.12345/contributors/567',
+                    helper.getRequest(SCOPE, 'update.json')
+                )
+                .reply(200, helper.getResponse(SCOPE, 'update.json'));
+
+            const result = await localiseAPI.contributors
+                .update('abcde.12345', 567, helper.getRequest(SCOPE, 'update.json'));
+            expect(result).to.eql(helper.getResponse(SCOPE, 'update.json'));
+        });
+    });
+
+    describe('delete', () => {
+        it('should remove contributor', async () => {
+            nock(helper.BASE_URL)
+                .delete('/projects/abcde.12345/contributors/567')
+                .reply(200, helper.getResponse(SCOPE, 'delete.json'));
+
+            const result = await localiseAPI.contributors.delete('abcde.12345', 567);
+            expect(result).to.eql(helper.getResponse(SCOPE, 'delete.json'));
+        });
+    });
+});
